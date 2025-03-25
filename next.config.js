684a6230ba54f48f1b0cc8db003a839e79128ca0
WinehouseD <kitdiana6@gmail.com>
2025-03-25 19:51:21 +0200

    add tanstack, tailwind

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/todo",
   reactStrictMode: true,
-  pageExtensions: ["jsx", "js"],
 };
 
 module.exports = nextConfig;
